d3c6438e3023e71c8d5c7b16e6b9f556c93c126a
Andrey Potapov <andrey.nautilus@yandex.ru>
2024-07-17 21:50:40 +0200

    feat(app-py): use unbuffered logging

--- app-py/main.py
+++ app-py/main.py
@@ -1,18 +1,22 @@
 import argparse
 from datetime import datetime
 from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
 
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        print(f"do_GET: {self.path}")
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=utf-8")
-            self.end_headers()
-            self.wfile.write(f"{self.server.page_message} {datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}".encode("utf-8"))
-        else:
-            self.send_error(404)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        page_line = [
+            self.server.page_message,
+            self.path,
+            datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+        ]
+        self.wfile.write(" - ".join(page_line).encode("utf-8"))
         
     def do_POST(self):
         pass
@@ -27,12 +31,12 @@ def main():
     if not args.message:
         raise RuntimeError("Message cannot be empty")
 
-    print(f"Message: {args.message}")
+    logging.info(f"Message: {args.message}")
 
     httpd = ThreadingHTTPServer(('', args.port), SimpleHandler)
     httpd.page_message = args.message
     
-    print(f"Start serving on port {args.port} ...")
+    logging.info(f"Start serving on port {args.port} ...")
     httpd.serve_forever()
 
 
