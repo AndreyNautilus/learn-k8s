af2af30ebcf2407a692c2bd5175ff64db25ca48d
Andrey Potapov <andrey.nautilus@yandex.ru>
2025-06-25 01:07:40 +0200

    fixup! feat(app): gunicorn for backend

--- app/backend/gunicorn.conf.py
+++ app/backend/gunicorn.conf.py
@@ -1,5 +1,1 @@
-from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
-
-
-def child_exit(server, worker):
-    GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
+# For now empty
