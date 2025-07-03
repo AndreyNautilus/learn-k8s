ea40575edb81504924d5eea4feaab1fe18327282
Andrey Potapov <andrey.nautilus@yandex.ru>
2025-07-04 00:37:36 +0200

    fix(db): escape quotes (#14)

--- app/db/test-data.sql
+++ app/db/test-data.sql
@@ -16,11 +16,11 @@ INSERT INTO posts (add_timestamp, author_id, message) VALUES
     current_timestamp() - 2,
     2,
     'If the game lets me raid, pillage, and crush my foes beneath my axe, '
-    'then its a game worthy of my time!'
+    'then it''s a game worthy of my time!'
 ),
 (
     current_timestamp() - 3,
     3,
-    'The sea calls louder than any landbound game — but if it’s got ships '
-    'and storms, you’ll find me at the helm!'
+    'The sea calls louder than any landbound game - but if it''s got ships '
+    'and storms, you''ll find me at the helm!'
 );
