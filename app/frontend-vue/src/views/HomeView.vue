5cd6c566c4f8cef224c49d5e114ea542729dc7dc
Andrey Potapov <andrey.nautilus@yandex.ru>
2025-08-17 19:59:16 +0200

    ci: frontend validation (#18)

--- app/frontend-vue/src/views/HomeView.vue
+++ app/frontend-vue/src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <!-- List of messages -->
     <div>
       <h2>List of existing messages</h2>
-      <div v-for="m in messages">
+      <div v-for="m in messages" :key="m.id">
         <MessageCard :author="m.author" :message="m.text" />
       </div>
     </div>
@@ -13,18 +13,6 @@
 <script>
 // @ is an alias to /src
 import MessageCard from "@/components/MessageCard.vue";
-
-// TODO: remove me, it's just for local checking
-const testData = [
-  {
-    author: "Fake User 1",
-    text: "Fake message 1",
-  },
-  {
-    author: "Fake User 2",
-    text: "Fake message 2",
-  },
-];
 
 export default {
   name: "HomeView",
